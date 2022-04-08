5fed48131ed4b86fec6286157fb4a46e841eaf8d
Lukasz Krulik <228780@edu.p.lodz.pl>
2022-04-08 13:28:51 +0200

    renaming test added 2nd testCase

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/productscatalog/ProductDataPublicBuilder.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/productscatalog/ProductDataPublicBuilder.java
@@ -6,9 +6,28 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 import java.util.Date;
 
 public class ProductDataPublicBuilder {
-    private static ProductData productData = new ProductData(Id.generate(),new Money(1),"testName",ProductType.DRUG,new Date());
+    private static Money money = new Money(1);
+    private static String name = "testName";
+    private static ProductType productType = ProductType.DRUG;
+    private static ProductData productData = new ProductData(Id.generate(),money,name,productType,new Date());
 
     public static ProductData getProductData() {
         return productData;
+    }
+
+    public static void setMoney(Money money) {
+        ProductDataPublicBuilder.money = money;
+    }
+
+    public static void setName(String name) {
+        ProductDataPublicBuilder.name = name;
+    }
+
+    public static void setProductType(ProductType productType) {
+        ProductDataPublicBuilder.productType = productType;
+    }
+
+    public static void setProductData(ProductData productData) {
+        ProductDataPublicBuilder.productData = productData;
     }
 }
--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -2,12 +2,14 @@ package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -26,12 +28,23 @@ class BookKeeperTest {
 
     //żądanie wydania faktury z jedną pozycją powinno zwrócić fakturę z jedną pozycją
     @Test
-    void TestCase1() {
+    void singleRequestShouldReturnSingleInvoice() {
         when(taxPolicy.calculateTax(any(), any())).thenReturn(this.testClass.getTax());
         testClass.addItemToRequest();
 
         BookKeeper bk = new BookKeeper(new InvoiceFactory());
         assertEquals(1, bk.issuance(this.testClass.getInvoiceRequest(), taxPolicy).getItems().size());
+    }
+
+    @Test
+    void doubleRequestShouldCallCalculateTaxTwice(){
+        when(taxPolicy.calculateTax(any(), any())).thenReturn(this.testClass.getTax());
+        testClass.addItemToRequest();
+        testClass.addItemToRequest();
+
+        BookKeeper bk = new BookKeeper(new InvoiceFactory());
+        bk.issuance(testClass.getInvoiceRequest(),taxPolicy);
+        Mockito.verify(taxPolicy, times(2)).calculateTax(any(), any());
     }
 
 }
