14791ec645bf1a7560a75f4c3eedc99faa58bdac
tomaszsztajkowski@gmail.com <tomaszsztajkowski@gmail.com>
2022-04-08 15:07:49 +0200

    added invoiceItemsSize

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -1,20 +1,52 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
+
+@ExtendWith(MockitoExtension.class)
 class BookKeeperTest {
+    static ClientData clientData;
+    static BookKeeper bookKeeper;
+    static Product product;
+    static InvoiceRequest invoiceRequest;
 
-    @BeforeEach
-    void setUp() throws Exception {
+    @Mock
+    static TaxPolicy taxPolicy;
+
+
+    @BeforeAll
+    static void setUp() {
+        bookKeeper = new BookKeeper(new InvoiceFactory());
+        clientData = new ClientData(Id.generate(), "Tomasz");
+        product = new Product(Id.generate(), new Money(10.20), "Cheese", ProductType.FOOD);
+        when(taxPolicy.calculateTax(ProductType.FOOD, product.getPrice()))
+                .thenReturn(new Tax(new Money(5), "test"));
 
     }
 
+    @BeforeEach
+    void setUpBeforeEach() {
+        invoiceRequest = new InvoiceRequest(clientData);
+    }
+
     @Test
-    void test() {
-        fail("not implemented");
+    void invoiceItemsSize() {
+        RequestItem requestItem = new RequestItem(product.generateSnapshot(), 1, product.getPrice());
+        invoiceRequest.add(requestItem);
+        assertEquals(1, bookKeeper.issuance(invoiceRequest, taxPolicy).getItems().size());
     }
 
 }
