79ad8b20b7b27f9c7d706cf2d59707606cdfdfed
karwowski.franciszek <228765@edu.p.lodz.pl>
2022-04-08 15:09:51 +0200

    added 2 tests

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -1,20 +1,75 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+@ExtendWith(MockitoExtension.class)
 class BookKeeperTest {
+    
+    private ClientData clientData;
+    private InvoiceRequest invoiceRequest;
+    private InvoiceFactory invoiceFactory;
+    private BookKeeper bookKeeper;
+    
+    @Mock
+    private TaxPolicy taxPolicy;
 
+    @BeforeAll
+    void setUP() throws Exception {
+        clientData = new ClientData(Id.generate(), "franek");
+        invoiceFactory = new InvoiceFactory();
+        bookKeeper = new BookKeeper(invoiceFactory);
+    }
+    
     @BeforeEach
     void setUp() throws Exception {
-
+        invoiceRequest = new InvoiceRequest(clientData);
     }
 
     @Test
-    void test() {
-        fail("not implemented");
+    void shouldReturnInvoiceWithOneItemWhenRequestInvoiceWithOneItem() {
+        Money money = new Money(100);
+        Product product = new Product(Id.generate(), new Money(100), "Marihuanen", ProductType.DRUG); // :)
+        ProductData productData = product.generateSnapshot();
+        RequestItem requestItem = new RequestItem(productData, 1, productData.getPrice());
+        invoiceRequest.add(requestItem);
+        
+        when(taxPolicy.calculateTax(ProductType.DRUG, money)).thenReturn(new Tax(new Money(12.2), "testTax"));
+        Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+        assertEquals(1, invoice.getItems().size());    
+    }
+
+    @Test
+    void shouldCallInvoiceTwoTimesWhenRequestInvoiceWithTwoItems() {
+        Money money = new Money(100);
+        Product product = new Product(Id.generate(), new Money(100), "Marihuanen", ProductType.DRUG); // :)
+        Product product1 = new Product(Id.generate(), new Money(150), "Cocainen", ProductType.DRUG); // :)
+        ProductData productData = product.generateSnapshot();
+        ProductData productData1 = product1.generateSnapshot();
+        RequestItem requestItem = new RequestItem(productData, 1, productData.getPrice());
+        RequestItem requestItem1 = new RequestItem(productData1, 1, productData1.getPrice());
+        invoiceRequest.add(requestItem);
+        invoiceRequest.add(requestItem1);
+        
+        when(taxPolicy.calculateTax(ProductType.DRUG, money)).thenReturn(new Tax(new Money(12.2), "testTax"));
+        bookKeeper.issuance(invoiceRequest, taxPolicy);
+        verify(taxPolicy, times(2)).calculateTax(ProductType.DRUG, money);
     }
 
 }
