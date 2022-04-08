f02eae6e2cccdf9a6ca808c00751b058236c0006
ImBioMario <m.sukiennik95@gmail.com>
2022-04-08 14:13:38 +0200

    Refactored test to make use of Mockito more... i think.

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -1,14 +1,21 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductDataBuilder;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
@@ -17,24 +24,34 @@ import java.util.Date;
 @ExtendWith(MockitoExtension.class)
 class BookKeeperTest {
 
+    @Mock
+    InvoiceFactory invoiceFactoryMock;
+    @Mock
+    TaxPolicy taxPolicyMock;
+
+    ClientData clientMock;
+    Tax taxMock;
+    RequestItem requestItem;
 
 
     @BeforeEach
     void setUp() throws Exception {
-
+        invoiceFactoryMock = mock(InvoiceFactory.class);
+        taxPolicyMock = mock(TaxPolicy.class);
+        clientMock = new ClientData(new Id("1"), "tester");
+        requestItem = new RequestItem(new ProductDataBuilder().build(), 1, new Money(100));
+        taxMock = new Tax(new Money(100), "testTax");
     }
 
     @Test
-    void test() {
-        InvoiceFactory invoiceFactory = new InvoiceFactory();
-        BookKeeper testKeeper = new BookKeeper(invoiceFactory);
-
-        ClientData cData = new ClientData(new Id("1"), "tester");
-        InvoiceRequest invoiceRequest = new InvoiceRequest(cData);
-        //invoiceRequest.add(new RequestItem(new ProductData(new Id("2"), new Money(2), "testProd", ProductType.FOOD, new Date())));
-
-        //Invoice correctInvoice  = testKeeper.issuance();
-
+    void invoiceWithOnePosition(){
+        when(invoiceFactoryMock.create(clientMock)).thenReturn(new Invoice(Id.generate(), clientMock));
+        BookKeeper bookKeeper = new BookKeeper(invoiceFactoryMock);
+        InvoiceRequest invoiceRequest = new InvoiceRequest(clientMock);
+        invoiceRequest.add(requestItem);
+        when(taxPolicyMock.calculateTax(any(), any())).thenReturn(taxMock);
+        Invoice testInvoice = bookKeeper.issuance(invoiceRequest, taxPolicyMock);
+        assertEquals(1, testInvoice.getItems().size());
     }
 
 }
